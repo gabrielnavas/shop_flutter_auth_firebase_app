5e45fda7e26720b62c5a2116e7599c0563d127b9
navas <gabrielnavas@email.com>
2024-03-08 17:51:24 -0300

    store class

---
+++ lib/data/store.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Store {
+  static Future<bool> saveString(String key, String value) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.setString(key, value);
+  }
+
+  static Future<bool> saveMap(String key, Map<String, dynamic> value) {
+    return saveString(key, jsonEncode(value));
+  }
+
+  static Future<String> getString(String key,
+      [String defaultValue = '']) async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? value = prefs.getString(key);
+    return value ?? defaultValue;
+  }
+
+  static Future<Map<dynamic, dynamic>> getMap(String key,
+      [Map<dynamic, dynamic> defaultValue = const {}]) async {
+    try {
+      return jsonDecode(await getString(key));
+    } catch (_) {
+      return defaultValue;
+    }
+  }
+
+  static Future<bool> remove(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.remove(key);
+  }
+}
